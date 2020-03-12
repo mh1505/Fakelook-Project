4eb0ee5fa257a4f5f34cdcf1635b604b14bfda92
mmShared <moty058770@gmail.com>
2020-03-12 10:06:16 +0200

    test commited

--- Fakelook-Server/connection/dbSql.js
+++ Fakelook-Server/connection/dbSql.js
@@ -13,17 +13,18 @@ function dbConnect() {
 };
 
 
-const executeQuery = function (res, procedure) {
-    sql.connect(dbConfig)
-    console.log('Connected to MSSQL')
-        .then(pool => {
-            return pool.request().execute(procedure)
-        }).then(result => {
-            res.json(result.recordset);
-        }).catch(err => {
-            res.json(err);
-        });
-}
+// const executeQuery = function (res, procedure) {
+//     sql.connect(dbConfig)
+//     console.log('Connected to MSSQL')
+//         .then(pool => {
+//             return pool.request().execute(procedure)
+//         }).then(result => {
+//             res.json(result.recordset);
+//         }).catch(err => {
+//             res.json(err);
+//         });
+// }
+aetbh
 
 
 
